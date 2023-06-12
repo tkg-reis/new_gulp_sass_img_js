d8d0027425824ff6575f8b0d5ec317386127df8c
Masataka Takaki <oorgns1uw5sk3ae@gmail.com>
2023-06-12 22:39:58 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+const { src, dest, series, watch } = require("gulp");
+const autoPrefixer = require("gulp-autoprefixer");
+const imageMin = require("gulp-imagemin").default;
+// const mozjpeg = require("imagemin-mozjpeg");
+const pngquant = require("imagemin-pngquant");
+const changed = require("gulp-changed");
+const uglify = require("gulp-uglify");
+const plumber = require("gulp-plumber");
+const purgecss = require("gulp-purgecss");
+const cleancss = require("gulp-clean-css");
+const sass = require("gulp-sass")(require("sass"));
+
+function defaultTask() {
+  return watch(["./css/*.scss", "./css/setting/*.scss"], () => {
+    return src("css/main.scss")
+      .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
+      .pipe(autoPrefixer())
+      .pipe(dest("css"));
+  });
+}
+
+exports.default = defaultTask;
+
+function imagemin(done) {
+  src("./img/**")
+    .pipe(changed("./dist/img/"))
+    .pipe(
+      imageMin([
+        pngquant({
+          quality: [0.6, 0.7],
+          speed: 1,
+        }),
+        // jpegはcommonjsの環境下でgulpでは使用が不可。ejs(import)形式でしか無理そう。
+//         mozjpeg({ quality: 65 }),
+        imageMin.svgo(),
+        imageMin.optipng({ optimizationLevel: 5 }),
+        imageMin.gifsicle(),
+      ])
+    )
+    .pipe(dest("./dist/img/"));
+
+  done();
+}
+
+exports.imagemin = imagemin;
+
+function js(done) {
+  src("./js/**/*.js")
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(dest("./dist/js/"));
+
+  done();
+}
+
+exports.js = js;
+
+function minify(done) {
+  src("./css/*.css")
+    .pipe(plumber())
+    .pipe(
+      purgecss({
+        content: ["./html/*.html", "./html/**/*.js"],
+      })
+    )
+    .pipe(cleancss())
+    .pipe(dest("./dist/css/"));
+
+  done();
+}
+
+exports.minify = minify;
+
---
+++ read.md
@@ -0,0 +1,7 @@
+## Attention!!!!
+
+## this repository is not support for jpeg/jpg to compact(imagemin-mozjpeg)
+
+## use software to compact like a tinyping and more....
+
+## thank you for reading
